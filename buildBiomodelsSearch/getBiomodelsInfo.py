bbe6478df1d113f20f32cde3c2d0bd58daddcb92
Bartholomew Jardine <70293946+bartjuw@users.noreply.github.com>
2023-12-05 09:30:12 -0800

    Add files to cash locally biomodels info
    A python script generates a json file containing biomodels info used for user search of models. Ideally this is faster than using the Biomodels Rest api to get the information

---
+++ buildBiomodelsSearch/getBiomodelsInfo.py
@@ -0,0 +1,35 @@
+# pip install biomodels-restful-api-client 
+
+import json
+import re
+from biomodels_restful_api_client import services as bmservices
+
+def remove_html_tags(text):
+    """Remove html tags from a string"""
+    import re
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
+
+
+totalModels = 1100
+i = 0
+modelIdentifiers = bmservices.get_model_identifiers()
+models = modelIdentifiers["models"]
+modelResults = {}
+for nModel in models:
+  if i < totalModels :
+    result = bmservices.get_model_info(nModel) 
+    if 'publicationId' in result:
+      modelNumber = result['publicationId']
+      if "BIOMD" in modelNumber:  # Only keep models with BIOMD number (curated model)
+        for key in result:
+          if key == "description":   
+            result[key] = remove_html_tags(result[key]) 
+           #print(result[key])
+        i+=1
+    #modelResults['model_'+ str(i)]= result
+        modelResults[modelNumber]= result
+
+#print(modelResults)
+with open('biomodelsinfo.json', 'w') as json_file:
+  json.dump(modelResults, json_file)
